bb76a4b93076d30f96e3792256e47d242ee5294c
yehudanevo <ynevo1948@gmail.com>
2021-09-13 20:03:37 +0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from function import *
 
-number_of_iterations = 0
-number_of_moves = 0
-
-iterations_tmp, moves_tmp = solve_queen_forword_checking(size)
-print('number of moves: ', number_of_moves, ' number of iteration : ', number_of_iterations)
+iterations, moves = solve_queen_forword_checking(size)
+print('number of moves: ', moves, ' number of iteration : ', iterations)
